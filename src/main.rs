01cf8fdab3dee7b805c1a81c01e32d7110905122
Cole Shanks <43489492+coleshanks@users.noreply.github.com>
2026-05-30 18:32:00 +0900

    refactored and added open_archive
    open_archive can be used by load_dict_index and load_terms seperately

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use monojiten::parser::load_dict_index;
+use monojiten::parser::{load_dict_index, open_archive};
 use std::path::Path;
 
 fn main() {
@@ -6,7 +6,12 @@ fn main() {
         "/Users/coleshanks/Documents/new_dic_shoui/[Monolingual] 明鏡国語辞典 第二版 Improved ver.zip",
     );
 
-    let dict_index = match load_dict_index(path) {
+    let mut archive = match open_archive(path) {
+        Ok(a) => a,
+        Err(e) => panic!("Couldn't open the archive\n{e}"),
+    };
+
+    let dict_index = match load_dict_index(&mut archive) {
         Ok(d) => d,
         Err(e) => panic!("Couldn't load the index\n{e}"),
     };
--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,6 @@
-// dictionary zips are made up of json files
-// this crate lets us convert that json data into struct instances
 use serde::Deserialize;
-
-use std::path::Path;
-
-// Deserialize TRAIT needs to be implemented on STRUCT DictIndex
-// impl Deserialize for DictIndex {} etc
-// using the derive attribute here. this logic can be auto generated
+use zip::ZipArchive;
+use std::{fs::File, path::Path};
 
 // dictionary metadata. Filename and format
 #[derive(Deserialize)]
@@ -17,44 +11,40 @@ pub struct DictIndex {
 
 // searchable ja word in the dic. 食べる has fields たべる, score, definition, etc
 #[derive(Deserialize)] // same thing as above
-pub struct Term {
-    pub word: String,                  // 食べる
+pub struct TermEntry {
+    pub term: String,                  // 食べる
     pub reading: String,               // たべる
-    pub score: i32,                    // 550
+    pub score: i64, // 550. 64 to handle potentially big scores plus matches serde json integer convention
     pub definition: serde_json::Value, // raw json from term bank. needs to be extracted later
 }
 
-// pub so main.rs can call load_dict_index. module items are by default private
-// this fn takes a path to a zip. returns the metadata for that dictionary (title and format)
-// 1 param. Path to the zip. Only borrows this data. Only needs to read not modify
-// we wrap the return value in Result<>. Result is an enum with two variants. Ok(T) or Err(E). So either we got the metadata no issues or we encountered a problem
-// the Ok(T) return value is DictIndex which is a struct with the metadata as fields
-// the Err(E) return case is wrapped in a Box<>. Box is a smart pointer. it is fixed size (just a pointer). but it points to the heap which is needed because a variety of errs could occur here. of varying sizes. so we point at the heap to accomodate
-// dyn is a keyword or qualifier. We could encounter a variety of errors. wrong format, no data, corrupt data, etc. We don't know which type it will be until runtime. dyn says i don't care which it is as long as it satisfies the Error trait from std. Figure it out at runtime and thats a valid return
-pub fn load_dict_index(path: &Path) -> Result<DictIndex, Box<dyn std::error::Error>> {
+pub fn open_archive(path: &Path) -> Result<ZipArchive<File>, Box<dyn std::error::Error>> {
     // let file = std::fs::File::open(path)?; is equivalent but we will be verbose for now
     let file = match std::fs::File::open(path) {
         Ok(f) => f,                     // on successful open. f is type struct File
         Err(e) => return Err(e.into()), // on unsuccessful open. we have error e, type struct Error from std::io. into method runs on e and converts it to our expected return error type. Box<dyn std::error::Error>
     };
 
-    // by_name changes the "cursor" for where archive is which is why it needs to be mut. index and file do not they only read
-    let mut archive = match zip::ZipArchive::new(file) {
-        Ok(a) => a, // on success. archive is a handle for the contents of the zip. we can now access json files etc
-        Err(e) => return Err(e.into()), // here we convert to Box<dyn Error> like above. in this case from zip::result::ZipError instead of std::io::Error but into method handles it the same
+    let archive = match zip::ZipArchive::new(file) {
+        Ok(f) => f,
+        Err(e) => return Err(e.into()),
     };
 
-    // readable handle to index.json inside the zip
+    Ok(archive)
+}
+
+pub fn load_dict_index(
+    archive: &mut ZipArchive<File>,
+) -> Result<DictIndex, Box<dyn std::error::Error>> {
     let index = match archive.by_name("index.json") {
         Ok(i) => i,
         Err(e) => return Err(e.into()),
     };
 
-    // deserialize index (json data) into dict_index (metadata struct)
     let dict_index = match serde_json::from_reader(index) {
         Ok(d) => d,
         Err(e) => return Err(e.into()),
     };
 
-    Ok(dict_index) // we return the metadata struct but wrapped in the Ok enum variant (if successful)
+    Ok(dict_index)
 }
